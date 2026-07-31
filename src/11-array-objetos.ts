3b4115b63b701c689bce79728175649f3bc04e10
Joao Paulo <jaozindasilva007@gmail.com>
2026-07-30 22:33:11 -0300

    aula 12 Generics

--- src/11-array-objetos.ts
+++ src/11-array-objetos.ts
@@ -1,37 +1,49 @@
-interface Filme {
-    titulo: string;
-    nota: number;
-    assitido: boolean
-}
+// interface Filme {
+//     titulo: string;
+//     nota: number;
+//     assitido: boolean
+// }
 
-const filmes: Filme[] = [
+// const filmes: Filme[] = [
 
-    {
-        titulo: 'Her',
-        nota: 9.6,
-        assitido: true
-    },
-    {
-        titulo: 'MIB',
-        nota: 8.5,
-        assitido: true
-    },
-    {
-        titulo: 'Livro de Eli',
-        nota: 8.4,
-        assitido: true
-    },
-    {
-        titulo: 'Vingadores',
-        nota: 9.7,
-        assitido: false
-    },
-    {
-        titulo: 'Detetive pikachu',
-        nota: 8,
-        assitido: false
-    },
-]
+//     {
+//         titulo: 'Her',
+//         nota: 9.6,
+//         assitido: true
+//     },
+//     {
+//         titulo: 'MIB',
+//         nota: 8.5,
+//         assitido: true
+//     },
+//     {
+//         titulo: 'Livro de Eli',
+//         nota: 8.4,
+//         assitido: true
+//     },
+//     {
+//         titulo: 'Vingadores',
+//         nota: 9.7,
+//         assitido: false
+//     },
+//     {
+//         titulo: 'Detetive pikachu',
+//         nota: 8,
+//         assitido: false
+//     },
+// ]
 
 
-console.log(filmes)
+// filmes.forEach((ani) => {
+//     console.log(ani.titulo)
+//     console.log(ani.nota)
+//     console.log(ani.assitido)
+// })
+
+// const animesAssitidos = filmes.filter(ani => ani.assitido === true)
+// console.log(animesAssitidos)
+
+// const titulos = filmes.map(fil => fil.titulo)
+
+// console.log(titulos)
+
