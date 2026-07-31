cf4a56b6f407b06a6bfaadbc12a1cc94b19acc44
Joao Paulo <jaozindasilva007@gmail.com>
2026-07-31 10:16:00 -0300

    Aula 14- Partial

--- src/13-enums.ts
+++ src/13-enums.ts
@@ -1,51 +1,51 @@
-enum Genero {
-    Romance = 'Romance',
-    Acao = 'Acao',
-    Drama = 'Drama',
-    Comedia = 'Comedia'
-}
+// enum Genero {
+//     Romance = 'Romance',
+//     Acao = 'Acao',
+//     Drama = 'Drama',
+//     Comedia = 'Comedia'
+// }
 
-type Status = "Em andamento" | "Finalizado" | "Pausado";
+// type Status = "Em andamento" | "Finalizado" | "Pausado";
 
-interface Filme {
-    titulo: string;
-    nota: number;
-    genero: Genero;
-    status: Status;
-}
+// interface Filme {
+//     titulo: string;
+//     nota: number;
+//     genero: Genero;
+//     status: Status;
+// }
 
-const her: Filme = {
-    titulo: 'Her',
-    nota: 9.6,
-    genero: Genero.Romance,
-    status: "Finalizado"
-}
+// const her: Filme = {
+//     titulo: 'Her',
+//     nota: 9.6,
+//     genero: Genero.Romance,
+//     status: "Finalizado"
+// }
 
-const mib: Filme = {
-    titulo: 'MIB',
-    nota: 8.8,
-    genero: Genero.Comedia,
-    status: "Finalizado"
-}
+// const mib: Filme = {
+//     titulo: 'MIB',
+//     nota: 8.8,
+//     genero: Genero.Comedia,
+//     status: "Finalizado"
+// }
 
-const titanic: Filme = {
-    titulo: 'Titanic',
-    nota: 9.7,
-    genero: Genero.Romance,
-    status: "Pausado"
-}
+// const titanic: Filme = {
+//     titulo: 'Titanic',
+//     nota: 9.7,
+//     genero: Genero.Romance,
+//     status: "Pausado"
+// }
 
 
 
-const mostrarFilmes = (filme: Filme): void => {
-    console.log(`Titulo: ${filme.titulo}`)
-    console.log(`Nota: ${filme.nota}`)
-    console.log(`Genero: ${filme.genero}`)
-    console.log(`Estado: ${filme.status}`)
-}
+// const mostrarFilmes = (filme: Filme): void => {
+//     console.log(`Titulo: ${filme.titulo}`)
+//     console.log(`Nota: ${filme.nota}`)
+//     console.log(`Genero: ${filme.genero}`)
+//     console.log(`Estado: ${filme.status}`)
+// }
 
-mostrarFilmes(her)
-mostrarFilmes(mib)
-mostrarFilmes(titanic)
+// mostrarFilmes(her)
+// mostrarFilmes(mib)
+// mostrarFilmes(titanic)
 
 
